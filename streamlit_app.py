b11ca780ad6008d39db6813a932b0c7034fe206c
msurl60061 <msurl60061@gmail.com>
2026-01-06 18:47:05 -0500

    Update streamlit_app.py
    import request corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
+      import requests
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
@@ -45,5 +46,5 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+
 
